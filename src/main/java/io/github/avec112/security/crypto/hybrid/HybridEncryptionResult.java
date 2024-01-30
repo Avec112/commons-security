a441b5da0bd5acca3fee4682eacc47ce60d4d251
Avec112 <avec112@gmail.com>
2024-01-30 09:20:40 +0100

    Add GitHub Actions version updater and update hybrid encryption result
    This commit adds a new GitHub Actions workflow, 'updater.yml', to automatically update the versions of used GitHub Actions every Sunday.

--- src/main/java/io/github/avec112/security/crypto/hybrid/HybridEncryptionResult.java
+++ src/main/java/io/github/avec112/security/crypto/hybrid/HybridEncryptionResult.java
@@ -2,11 +2,12 @@ package io.github.avec112.security.crypto.hybrid;
 
 import io.github.avec112.security.crypto.aes.EncryptionMode;
 import io.github.avec112.security.crypto.aes.EncryptionStrength;
+import io.github.avec112.security.crypto.domain.CipherText;
 import lombok.Value;
 
 @Value
 public class HybridEncryptionResult {
-    String cipherText;
+    CipherText cipherText;
     String encryptedSymmetricalKey;
     EncryptionMode aesEncryptionMode;
     EncryptionStrength aesEncryptionStrength;
