2aa39e9b74f9fa28ca585aa5b6acf4094fcdc921
Avec112 <avec112@gmail.com>
2025-10-26 13:45:49 +0100

    Add EqualsVerifier tests for Share and Secret classes
    - Introduced `ShareTest` and `SecretTest` with EqualsVerifier to ensure equals and hashCode contract compliance.
    - Added EqualsVerifier dependency to `pom.xml` for testing.

---
+++ src/test/java/com/github/avec112/security/crypto/shamir/SecretTest.java
@@ -0,0 +1,12 @@
+package com.github.avec112.security.crypto.shamir;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+class SecretTest {
+
+    @Test
+    public void simpleEqualsContract() {
+        EqualsVerifier.forClass(Secret.class).verify();
+    }
+}
